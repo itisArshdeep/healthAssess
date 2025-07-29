b888b84bb2ea332c3159ab5e838bdc35b95f08d1
Arshdeep Singh <deepsingh13131212@gmail.com>
2025-07-29 18:10:54 +0530

    success

--- src/Pages/FeaturePage/FeaturePage.jsx
+++ src/Pages/FeaturePage/FeaturePage.jsx
@@ -6,7 +6,15 @@ import { Link } from 'react-router-dom'
 const FeaturePage = () => {
     return (
         <div>
-            <Header />
+            <div className='flex justify-between h-18 px-4 items-center bg-gradient-to-r rounded-b-[30px] shadow-gray-300 shadow-2xl  from-[#70D9BF] to-[#75FB8A]'>
+                        <Link to="/ ">
+                            <img className='h-4' src="https://img.icons8.com/ios-filled/50/long-arrow-left.png" alt="" />
+                        </Link>
+                        <div>
+                            <h3 className='font-semibold text-[18px] '>Exercise List</h3>
+                        </div>
+                        <div></div>
+                    </div>
             <div>
                 <Link to="addtimer"><div className='flex justify-between items-center p-4 mx-5 shadow-2xl mt-12 shadow-gray-200 rounded-[12px]'>
                     <div className='flex gap-x-5'>
