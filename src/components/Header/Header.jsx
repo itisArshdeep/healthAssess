b002bcd9a27550643417ce37a5b8ed595f1f7eb4
Arshdeep Singh <deepsingh13131212@gmail.com>
2025-07-29 18:05:33 +0530

    success

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <div className='flex justify-between h-18 px-4 items-center bg-gradient-to-r rounded-b-[30px] shadow-gray-300 shadow-2xl  from-[#70D9BF] to-[#75FB8A]'>
-            <Link to="/ ">
+            <Link to="/features ">
                 <img className='h-4' src="https://img.icons8.com/ios-filled/50/long-arrow-left.png" alt="" />
             </Link>
             <div>
