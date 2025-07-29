658b6eb7f9f6a6f5fc75b4d97d9515e4cce741bc
Arshdeep Singh <deepsingh13131212@gmail.com>
2025-07-29 18:08:16 +0530

    success

--- src/Pages/ExercisePage/Exercise.jsx
+++ src/Pages/ExercisePage/Exercise.jsx
@@ -6,7 +6,15 @@ import { Link } from 'react-router-dom'
 const Exercise = () => {
     return (
         <div className=''>
-            <Header />
+             <div className='flex justify-between h-18 px-4 items-center bg-gradient-to-r rounded-b-[30px] shadow-gray-300 shadow-2xl  from-[#70D9BF] to-[#75FB8A]'>
+                        <Link to="/ ">
+                            <img className='h-4' src="https://img.icons8.com/ios-filled/50/long-arrow-left.png" alt="" />
+                        </Link>
+                        <div>
+                            <h3 className='font-semibold text-[18px] '>Exercise List</h3>
+                        </div>
+                        <div></div>
+                    </div>
             <div className='mt-6 flex gap-3 flex-col'>
                 <ExerciseCards image={"https://thumbs.dreamstime.com/z/woman-doing-quadriceps-stretch-cool-down-exercise-woman-doing-quadriceps-stretch-cool-down-exercise-balance-pose-flexibility-157558626.jpg?ct=jpeg"} name={"Hip Flexion"} />
                 <ExerciseCards image={"https://thumbs.dreamstime.com/z/woman-doing-quadriceps-stretch-cool-down-exercise-woman-doing-quadriceps-stretch-cool-down-exercise-balance-pose-flexibility-157558626.jpg?ct=jpeg"} name={"Hip Flexion"} />
